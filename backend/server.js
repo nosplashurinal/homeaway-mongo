5a08cb4c7b31f30dff37e55fdee367d2d75c5efb
kailash <kailash.ramakrishna@gmail.com>
2018-11-08 11:15:49 -0800

    changed login

--- backend/server.js
+++ backend/server.js
@@ -78,6 +78,57 @@ const uploadFile = (buffer, name, type) => {
   return s3.upload(params).promise();
 };
 
+app.post("/Login", (req, res, next) => {
+  console.log("Inside login request");
+  if (!req.body.email || !req.body.password) {
+    res.status(404).send("Email/Password fields cannot be blank!");
+  } else {
+    passport.authenticate("local", { session: false }, (error, user) => {
+      if (error || !user) {
+        console.log("Authentication error", error);
+        res.status(401).send(error);
+        return;
+      }
+
+      console.log("User Login Successful! Response from authenticate", user);
+      /** This is what ends up in our JWT */
+      console.log("Firstname : ", user.firstname);
+      const payload = {
+        email: user.email,
+        userid: user._id,
+        firstname: user.firstname,
+        expires: Date.now() + parseInt(process.env.JWT_EXPIRATION_MS)
+      };
+
+      /** assigns payload to req.user */
+      req.login(payload, { session: false }, error => {
+        if (error) {
+          console.log(error);
+          res.status(400).send(error);
+        }
+
+        /** generate a signed json web token and return it in the response */
+        const token = jwt.sign(JSON.stringify(payload), config.secret);
+
+        /** assign our jwt to the cookie */
+        res.cookie("jwt", token, {
+          maxAge: 900000,
+          //expires: new Date(Date.now() + 900000),
+          httpOnly: false,
+          path: "/"
+        });
+        res.status(200).json({
+          email: user.email,
+          firstname: user.firstname,
+          lastname: user.lastname,
+          type: user.type,
+          _id: user._id
+        });
+      });
+    })(req, res, next);
+  }
+});
+
 //Login Route (needs to be moved to express route)
 
 // app.post("/Login", (req, res, next) => {
@@ -136,7 +187,7 @@ app.post("/PhotoUpload", (req, res) => {
   });
 });
 
-app.use("/Login", require("./routes/login"));
+//app.use("/Login", require("./routes/login"));
 app.use("/Register", require("./routes/register"));
 app.use("/PropertyList", require("./routes/property-list"));
 app.use("/Property", require("./routes/property-details"));
