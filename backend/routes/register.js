af285fdc7850d3047cde684e70877f85dbbac6cc
kailash <kailash.ramakrishna@gmail.com>
2018-11-08 19:20:02 -0800

    validation

--- backend/routes/register.js
+++ backend/routes/register.js
@@ -9,10 +9,10 @@ router.post("/", (req, res) => {
 
     if (err) {
       console.log("Error creating user!", err);
-      res.sendStatus(400).end();
+      res.status(400).end();
     } else if (results.success === "fail") {
       console.log("Email already exists!");
-      res.status(400).end();
+      res.status(400).send("Email already exists!");
     } else {
       console.log("User created : ", results);
       res.status(200).json({ results });
